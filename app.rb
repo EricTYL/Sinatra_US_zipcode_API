068a0956af7402c21d1b7af3bab9bc2896653403
RickyTYL <rickytyl@icloud.com>
2015-11-18 12:32:27 +0800

    Demo API in json

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'data_mapper'
 require './models/zipcode.rb'
+require 'json'
 
 DataMapper.setup(:default, 'postgres://localhost/us_zipcode')
+DataMapper.finalize.auto_upgrade!
 
 get '/zipcode/:id' do
-  @zipcode = Zipcode.get(params[:id])
-  erb :zipcode_view
+  content_type :json
+  zipcode = Zipcode.get(params[:id])
+  { zip: zipcode.zip, city: zipcode.city, state: zipcode.state, lat: zipcode.lat, lng: zipcode.lng }.to_json
+  #erb :zipcode_view
 end
-
-DataMapper.finalize.auto_upgrade!
