ae26c15c094e92e0a08a9f3f199d7f5a7b2cf13a
RickyTYL <rickytyl@icloud.com>
2015-11-18 12:23:04 +0800

    Add API's web view

--- app.rb
+++ app.rb
@@ -3,4 +3,10 @@ require 'data_mapper'
 require './models/zipcode.rb'
 
 DataMapper.setup(:default, 'postgres://localhost/us_zipcode')
+
+get '/zipcode/:id' do
+  @zipcode = Zipcode.get(params[:id])
+  erb :zipcode_view
+end
+
 DataMapper.finalize.auto_upgrade!
